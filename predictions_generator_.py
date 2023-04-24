06e494ddd4c7dbaedcf512a0a9d5c17184aa9b7f
Danylo Nedosieka <darituan8@gmail.com>
2023-04-24 13:36:04 +0300

    Wrap prediction generation in a function

--- predictions_generator_.py
+++ predictions_generator_.py
@@ -1,6 +1,10 @@
 import csv
 import pickle
 import pandas as pd
+import json
+import requests
+import os
+from datetime import datetime
 
 INPUT_DATA_FOLDER = "lab3/data/modified"
 MODELS_FOLDER="lab3/models"
@@ -23,7 +27,7 @@ for i in range(len(vectors)):
 vectors = []
 
 
-with open(f"{INPUT_DATA_FOLDER}/{REGION_FILE}", newline='') as csvfile:
+with open(f"{INPUT_DATA_FOLDER}/{REGION_FILE}", newline='', encoding='utf-8') as csvfile:
     regions = list(csv.reader(csvfile))
 
 with open(f"{INPUT_DATA_FOLDER}/{DATES_FILE}", 'rb') as datafile:
@@ -32,9 +36,9 @@ with open(f"{INPUT_DATA_FOLDER}/{DATES_FILE}", 'rb') as datafile:
 for i in range(len(dates)):
     dates[i] = pd.to_datetime(dates[i].replace(".txt", ""), format="%d:%m:%y")
 dates = sorted(dates)
-#%%
-import pickle
-import pandas as pd
+
+
+pickled_model = pickle.load(open(f'{MODELS_FOLDER}/{MODEL_FILE}', 'rb'))
 
 
 # addition funcs
@@ -44,6 +48,7 @@ def findRegion(check):
             if regionTemp == check:
                 return region[4]
     return None
+
 
 def findRegionName(regiond_id):
     for region in regions:
@@ -81,130 +86,123 @@ def getAlarmByDate(location, date, alarmsDict):
         started = not started
     return 0
 
-#%%
-import json
-import requests
 
 url = "http://localhost:8080/forecast"
 
-response = requests.get(url)
 
-if response.status_code == 200:
-    data = json.loads(response.text)
-    #print(data)
-else:
-    print(f"Error: {response.status_code}")
-    exit()
+def generate_predictions():
+    response = requests.get(url)
 
-#%%
-import pickle
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        # print(data)
+    else:
+        print(f"Error: {response.status_code}")
+        exit()
 
-test_data = []
-prediction_descriptions=[]
-for line in data['data']:
-    words = line.split(',')
-    test_data.append(words)
-    desc=[words[0]]
-    prediction_descriptions.append([findRegionName(words[0]),words[17][0:5]])
-#%%
+    test_data = []
+    prediction_descriptions = []
+    for line in data['data']:
+        words = line.split(',')
+        test_data.append(words)
+        desc = [words[0]]
+        prediction_descriptions.append([findRegionName(words[0]), words[17][0:5]])
 
-alarmsBit = []
-dayConditions = {}
-hourConditions = {}
-dayConditionsIndex = 16
-hourConditionsIndex = 29
-indexsToRemove = []
-for i in range(len(test_data)):
-    if i % 10000 == 0:
-        print(i)
-    x = test_data[i]
-    aaa = findRegion(x[0].split(",")[0])
-    if aaa is None:
-        print(x)
-    x[0] = aaa
-    date = pd.to_datetime(x[1])
-    # if not findDate(dates, date):
-    #     indexsToRemove.append(i - len(indexsToRemove))
-    #     continue
-    #     remove  time
+    alarmsBit = []
+    dayConditions = {}
+    hourConditions = {}
+    dayConditionsIndex = 16
+    hourConditionsIndex = 29
+    indexsToRemove = []
+    for i in range(len(test_data)):
+        if i % 10000 == 0:
+            print(i)
+        x = test_data[i]
+        aaa = findRegion(x[0].split(",")[0])
+        if aaa is None:
+            print(x)
+        x[0] = aaa
+        date = pd.to_datetime(x[1])
+        # if not findDate(dates, date):
+        #     indexsToRemove.append(i - len(indexsToRemove))
+        #     continue
+        #     remove  time
 
-    x.pop(17)
-    try:
-        x[dayConditionsIndex] = dayConditions[x[dayConditionsIndex]]
-    except (KeyError):
-        dayConditions[x[dayConditionsIndex]] = len(dayConditions) + 1
-        x[dayConditionsIndex] = dayConditions[x[dayConditionsIndex]]
+        x.pop(17)
+        try:
+            x[dayConditionsIndex] = dayConditions[x[dayConditionsIndex]]
+        except (KeyError):
+            dayConditions[x[dayConditionsIndex]] = len(dayConditions) + 1
+            x[dayConditionsIndex] = dayConditions[x[dayConditionsIndex]]
 
-    try:
-        x[hourConditionsIndex] = hourConditions[x[hourConditionsIndex]]
-    except (KeyError):
-        hourConditions[x[hourConditionsIndex]] = len(hourConditions) + 1
-        x[hourConditionsIndex] = hourConditions[x[hourConditionsIndex]]
+        try:
+            x[hourConditionsIndex] = hourConditions[x[hourConditionsIndex]]
+        except (KeyError):
+            hourConditions[x[hourConditionsIndex]] = len(hourConditions) + 1
+            x[hourConditionsIndex] = hourConditions[x[hourConditionsIndex]]
 
-    #     remove date
-    x.pop(1)
-    # vector = findVector(vectorsFloat, dates, date)
-    # if vector is None:
-    #     print(date)
-    #     continue
-    # x.extend(vector)
+        #     remove date
+        x.pop(1)
+        # vector = findVector(vectorsFloat, dates, date)
+        # if vector is None:
+        #     print(date)
+        #     continue
+        # x.extend(vector)
 
-#     weather.append(temp.copy())
-dayConditions = {}
-dayIcons = {}
-hourConditions = {}
-hourIcons = {}
-alarmsDict = {}
-vectorsFloat = []
+    #     weather.append(temp.copy())
+    dayConditions = {}
+    dayIcons = {}
+    hourConditions = {}
+    hourIcons = {}
+    alarmsDict = {}
+    vectorsFloat = []
 
-#%%
-print(len(indexsToRemove))
-print(len(test_data))
-for index in indexsToRemove:
-    test_data.pop(index)
+    print(len(indexsToRemove))
+    print(len(test_data))
+    for index in indexsToRemove:
+        test_data.pop(index)
+
+    for x in test_data:
+        for i in range(len(x)):
+            if x[i] == '':
+                x[i] = 0.0
+            if type(x[i]) is str:
+                x[i] = float(x[i])
+            if type(x[i]) is int:
+                x[i] = float(x[i])
+    for x in test_data:
+        for n in range(6748 - len(x)):
+            x.append(0.0)
+
+    prediction = pickled_model.predict(test_data)
+
+    now = datetime.utcnow()
+    result = {}
+    # formatted_result['regions_forecast']={}
+    for n in range(len(prediction)):
+        prediction[n] = round(prediction[n])
+        alarm_expected = bool(prediction[n] == 1)
+        place = prediction_descriptions[n][0]
+        time = prediction_descriptions[n][1]
+        if place not in result:
+            result[place] = {}
+        result[place][time] = alarm_expected
+
+    modified_time = os.path.getmtime(f"{MODELS_FOLDER}/{MODEL_FILE}")
+
+    modified_datetime = datetime.utcfromtimestamp(modified_time)
+
+    formatted_date = modified_datetime.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    formatted_result = {
+        "last_model_train_time": formatted_date,
+        "last_prediction_time": now.strftime("%Y-%m-%dT%H:%M:%SZ"),
+        "regions_forecast": result
+    }
+    with open("predictions_all.json", "w") as f:
+        # Write the dictionary to the file in JSON format
+        json.dump(formatted_result, f)
 
 
-for x in test_data:
-    for i in range(len(x)):
-        if x[i] == '':
-            x[i] = 0.0
-        if type(x[i]) is str:
-            x[i] = float(x[i])
-        if type(x[i]) is int:
-            x[i] = float(x[i])
-for x in test_data:
-    for n in range(6748-len(x)):
-        x.append(0.0)
-#%%
-
-pickled_model = pickle.load(open(f'{MODELS_FOLDER}/{MODEL_FILE}', 'rb'))
-prediction=pickled_model.predict(test_data)
-#%%
-import os
-from datetime import datetime
-now = datetime.utcnow()
-result={}
-#formatted_result['regions_forecast']={}
-for n in range(len(prediction)):
-    prediction[n]=round(prediction[n])
-    alarm_expected=bool(prediction[n]==1)
-    place=prediction_descriptions[n][0]
-    time=prediction_descriptions[n][1]
-    if place not in result:
-        result[place]={}
-    result[place][time]=alarm_expected
-
-modified_time = os.path.getmtime(f"{MODELS_FOLDER}/{MODEL_FILE}")
-
-modified_datetime = datetime.utcfromtimestamp(modified_time)
-
-formatted_date = modified_datetime.strftime("%Y-%m-%dT%H:%M:%SZ")
-
-formatted_result={
-    "last_model_train_time":formatted_date,
-    "last_prediction_time":now.strftime("%Y-%m-%dT%H:%M:%SZ"),
-    "regions_forecast":result
-}
-with open("predictions_all.json", "w") as f:
-    # Write the dictionary to the file in JSON format
-    json.dump(formatted_result, f)
+if __name__ == '__main__':
+    generate_predictions()
